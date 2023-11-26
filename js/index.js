00a213aa576e282184cd9e84eadd7f411f08fe28
eslam-obeidat <146520433+eslam-obeidat@users.noreply.github.com>
2023-11-26 12:50:08 -0800

    html and css

---
+++ js/index.js
@@ -0,0 +1,38 @@
+var firstNameForm = document.getElementById('firstName');
+var lastNameForm = document.getElementById('lastName');
+var phoneNumberForm = document.getElementById('phoneNumber');
+var confirmPhoneNumberForm = document.getElementById('confirmPhoneNumber');
+regForm.addEventListener('submit',function(e) {
+    e.preventDefault();
+    if (phoneNumberForm.value!=confirmPhoneNumberForm.value) { 
+
+        Swal.fire ({ 
+            position: 'center',
+            icon: 'error',
+            title: 'PhoneNumber and Confirm PhoneNumber are not the same',
+            showConfirmButton: false,
+            timer: 6000
+        });
+
+        // location.replace("../index.html");
+      
+
+    }
+    else{
+       
+        localStorage.setItem( 'firstName', firstNameForm.value);
+        localStorage.setItem( 'lastName',lastNameForm.value);
+        localStorage.setItem( 'phoneNumber',phoneNumberForm.value);
+        localStorage.setItem( 'confirmPhoneNumber',confirmPhoneNumberForm.value);
+        
+        Swal.fire({
+            position: 'center',
+            icon: "success",
+            title: 'Your account was created successfully',
+            showConfirmButton: false,
+            timer: 6000
+          });
+        }
+        // window.location ="./pages/home.html";
+        window.location ="../index.html";
+});
